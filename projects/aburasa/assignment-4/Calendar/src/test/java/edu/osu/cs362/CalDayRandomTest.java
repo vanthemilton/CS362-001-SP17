e511a15cf4f547d33a48744a37a25ee90cf5f14f
Ali Aburas <aburasa@nondalton.eecs.oregonstate.edu>
2017-05-15 23:58:45 -0700

    assignment-4 is added

---
+++ projects/aburasa/assignment-4/Calendar/src/test/java/edu/osu/cs362/CalDayRandomTest.java
@@ -0,0 +1,29 @@
+package edu.osu.cs362;
+
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for CalDay class.
+ */
+
+public class CalDayRandomTest {
+	
+    /**
+     * Generate Random Tests that tests CalDay Class.
+     */
+	 @Test
+	  public void radnomtest()  throws Throwable  {
+		 
+		 
+		 
+	 }
+
+
+	
+}
---
+++ projects/aburasa/assignment-4/Calendar/src/test/java/edu/osu/cs362/TimeTableRandomTest.java
@@ -0,0 +1,31 @@
+package edu.osu.cs362;
+
+import java.util.Calendar;
+import java.util.Random;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+
+
+/**
+ * Random Test Generator  for TimeTable class.
+ */
+
+public class TimeTableRandomTest {
+	
+    /**
+     * Generate Random Tests that tests TimeTable Class.
+     */
+	 @Test
+	  public void radnomtest()  throws Throwable  {
+		 
+		 
+		 
+	 }
+
+
+	
+}
